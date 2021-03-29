2d806002a69e3fff92d40238ca6ed15b8e2eb1e1
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-03-29 12:46:08 +0200

    add Arrays-DS problem solution

---
+++ Hackerrank/DataStructures/Arrays/Arrays_DS.cs
@@ -0,0 +1,20 @@
+﻿namespace DataStructures.Arrays
+{
+    class Arrays_DS
+    {
+        static int[] ReverseArray(int[] a)
+        {
+            int length = a.Length;
+            // a.Reverse();
+
+            for (int i = 0; i < length / 2; i++)
+            {
+                int temp = a[i];
+                a[i] = a[length - (i + 1)];
+                a[length - (i + 1)] = temp;
+            }
+
+            return a;
+        }
+    }
+}
