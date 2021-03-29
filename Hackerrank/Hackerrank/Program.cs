ea87037aa1e4f2d2f1e0e44b236df8b06be7e5cb
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-03-29 12:35:42 +0200

    initial commit

---
+++ Hackerrank/Hackerrank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hackerrank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
