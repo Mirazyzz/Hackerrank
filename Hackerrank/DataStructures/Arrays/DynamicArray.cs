d135dda45db2e9cd444174cd872dd100ae69226e
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-04-26 15:46:10 +0200

    add solution for dynamic arrays

---
+++ Hackerrank/DataStructures/Arrays/DynamicArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Arrays
+{
+    class DynamicArray
+    {
+        public static List<int> Dynamic(int n, List<List<int>> queries)
+        {
+            List<List<int>> dArr = new List<List<int>>();
+            List<int> result = new List<int>();
+            int lastAnswer = 0;
+
+            // Initialize sub "arrays"
+            for (int i = 0; i < n; i++)
+            {
+                dArr.Add(new List<int>());
+            }
+
+            foreach (List<int> query in queries)
+            {
+                // Get the index of element in the array
+                // for current iteration
+                int idx = ((query[1] ^ lastAnswer) % n);
+
+                if (query[0] == 1)
+                {
+                    // Append "y" to the sublist at index "idx"
+                    dArr[idx].Add(query[2]);
+                }
+                else
+                {
+                    // Get value of lastAnswer from sublist under index
+                    // found from formula
+                    lastAnswer = dArr[idx][query[2] % dArr[idx].Count];
+
+                    result.Add(lastAnswer);
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- Hackerrank/DataStructures/Program.cs
+++ Hackerrank/DataStructures/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataStructures.Arrays;
+using System;
+using System.Collections.Generic;
 
 namespace DataStructures
 {
@@ -6,7 +8,42 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<List<int>> query = new List<List<int>>
+            {
+                new List<int>
+                {
+                    1,
+                    0,
+                    5
+                },
+                new List<int>
+                {
+                    1,
+                    1,
+                    7
+                },
+                new List<int>
+                {
+                    1,
+                    0,
+                    3
+                },
+                new List<int>
+                {
+                    2,
+                    1,
+                    0
+                },
+                new List<int>
+                {
+                    2,
+                    1,
+                    1
+                },
+            };
+
+            DynamicArray.Dynamic(2, query);
+
         }
     }
 }
