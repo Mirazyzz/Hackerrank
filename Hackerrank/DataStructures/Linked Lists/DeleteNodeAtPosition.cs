e97b3bd9abc374b2fd3ae6eaac47ebcae14e70b6
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-05-02 15:54:29 +0200

    add delete node at solution

---
+++ Hackerrank/DataStructures/Linked Lists/DeleteNodeAtPosition.cs
@@ -0,0 +1,24 @@
+﻿namespace DataStructures.Linked_Lists
+{
+    class DeleteNodeAtPosition
+    {
+        public static SinglyLinkedListNode DeleteNode(SinglyLinkedListNode head, int position)
+        {
+            int currentPosition = 1;
+            var temp = head;
+
+            while(temp != null && position != currentPosition)
+            {
+                temp = temp.next;
+                currentPosition++;
+            }
+            
+            if(temp.next != null)
+            {
+                temp.next = temp.next.next;
+            }
+
+            return head;
+        }
+    }
+}
