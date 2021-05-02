8a3811cc0daaf57d3c8458579a930dc0a0c36fbc
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-05-02 15:49:42 +0200

    add insert node at position solution

---
+++ Hackerrank/DataStructures/Linked Lists/InsertNodeAtPosition.cs
@@ -0,0 +1,25 @@
+﻿namespace DataStructures.Linked_Lists
+{
+    class InsertNodeAtPosition
+    {
+        public static SinglyLinkedListNode InsertNode(SinglyLinkedListNode head, int data, int position)
+        {
+            if (head == null) return new SinglyLinkedListNode(data);
+
+            int currentPosition = 1;
+            var temp = head;
+
+            while (temp != null && currentPosition != position)
+            {
+                temp = temp.next;
+                currentPosition++;
+            }
+
+            var newNode = new SinglyLinkedListNode(data);
+            newNode.next = temp.next;
+            temp.next = newNode;
+
+            return head;
+        }
+    }
+}
