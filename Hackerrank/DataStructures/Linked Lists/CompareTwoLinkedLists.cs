29c4f478cdc6f0c2eab5e6f8e17fad9a91da77b9
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-05-04 18:06:54 +0200

    add compare two linked lists

---
+++ Hackerrank/DataStructures/Linked Lists/CompareTwoLinkedLists.cs
@@ -0,0 +1,17 @@
+﻿namespace DataStructures.Linked_Lists
+{
+    class CompareTwoLinkedLists
+    {
+        public static bool Compare(SinglyLinkedListNode head1, SinglyLinkedListNode head2)
+        {
+            while(head1 != null && head2 != null)
+            {
+                if (head1.data != head2.data) return false;
+            }
+
+            if (head1 != null || head2 != null) return false;
+
+            return true;
+        }
+    }
+}
